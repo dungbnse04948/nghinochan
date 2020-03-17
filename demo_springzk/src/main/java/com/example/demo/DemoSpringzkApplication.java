cba311e5ad09c07daa0fc03cf413910420af31f7
dungbnse04948 <47481644+dungbnse04948@users.noreply.github.com>
2020-03-17 09:24:06 +0700

    Add files via upload

---
+++ demo_springzk/src/main/java/com/example/demo/DemoSpringzkApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringzkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringzkApplication.class, args);
+		System.out.println("alo alo");
+	}
+
+}
