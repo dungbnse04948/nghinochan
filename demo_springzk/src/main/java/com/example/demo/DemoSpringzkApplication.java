ef855f38b9964ddd8841b8c5ec664fcba6fe69c3
dungbnse04948 <dungbnse04948@fpt.edu.vn>
2020-03-17 10:15:31 +0700

    nghi no chan

--- demo_springzk/src/main/java/com/example/demo/DemoSpringzkApplication.java
+++ demo_springzk/src/main/java/com/example/demo/DemoSpringzkApplication.java
@@ -2,13 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.zkoss.zul.South;
 
 @SpringBootApplication
 public class DemoSpringzkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringzkApplication.class, args);
-		System.out.println("alo alo");
+		System.out.println("alo alo zzz");
+		System.out.println("alo alo zzz1");
+		
 	}
 
 }
